f0f1064ebfc84e69ff24c0a8a8c2371e1920fa36
tmddn5645 <tmddn5645@gmail.com>
2025-11-21 20:02:45 +0900

    Initial commit

---
+++ Chapter1_1/Chapter1_1/main_chapter1_1.cpp
@@ -0,0 +1,19 @@
+/*
+	This is my program.
+	Written by me.
+*/
+
+#include <iostream>
+
+int main(void)
+{
+	int x = 2;
+	x = 5;
+	int y = x + 3;
+
+	std::cout << y << std::endl;
+
+
+
+	return 0;
+}
---
+++ Chapter1_1/Chapter1_3/main_1_3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+	int x = 123; // initialization
+	x = 123; // '=' : assignment operater
+
+	std::cout << x << std::endl;
+	std::cout << &x << std::endl;
+
+	return 0;
+}
