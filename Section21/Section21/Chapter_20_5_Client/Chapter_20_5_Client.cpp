da946ddb26d541d7165c7f70e6adf0a447785c8c
tmddn5645 <tmddn5645@gmail.com>
2026-03-04 23:20:52 +0900

    Chapter_20_5_Client
    Chapter_20_5_Client

--- Section21/Section21/Chapter_20_5_Client/Chapter_20_5_Client.cpp
+++ Section21/Section21/Chapter_20_5_Client/Chapter_20_5_Client.cpp
@@ -4,7 +4,36 @@
 
 using boost::asio::ip::tcp;
 
-int main(int arg, char* argv[])
+int main(int argc, char* argv[])
 {
+	try
+	{
+		if (argc != 2)
+		{
+			std::cerr << "Usage: client <host>" << std::endl;
+			return 1;
+		}
 
+		tcp::iostream stream(argv[1], std::to_string(int(13))); // stream(address 127.0.0.1, port number 13)
+		if (!stream)
+		{
+			std::cout << "Unable to connect: " << stream.error().message() << std::endl;
+			return 1;
+		}
+
+		// send message to server
+		stream << "Hello from Client";
+		stream << std::endl; // send new-line to end receiver'stream getline
+
+		// receive message from server
+		std::string line;
+		std::getline(stream, line);
+		std::cout << line << std::endl;
+	}
+	catch (std::exception& e)
+	{
+		std::cout << "Exception" << e.what() << std::endl;
+	}
+
+	return 0;
 }
